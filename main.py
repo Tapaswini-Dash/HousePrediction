250e83ac64a8daa4b3d568d3f08af68b77e601e9
Tapaswini Dash <tapaswinidash@gmail.com>
2026-02-25 12:58:31 +0530

    Fix CORS typo and update origins

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="House Price Predictor")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["https://house-price-frontend-eta.vercel.ap", "http://127.0.0.1:5173 , "http://localhost:5173""],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
