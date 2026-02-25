85bf7563c3b406de0dd1d117253a3578ff569484
Tapaswini Dash <tapaswinidash@gmail.com>
2026-02-25 12:59:40 +0530

    Corrected API endpoint path to /predict

--- house-price-frontend/src/Components/LandingPage.jsx
+++ house-price-frontend/src/Components/LandingPage.jsx
@@ -26,7 +26,7 @@ function LandingPage() {
     setPrediction(null)
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch('https://houseprediction-2-836p.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
